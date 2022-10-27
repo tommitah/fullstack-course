703c39d904c2352aa07c6f101f43c0bf0a64ccb4
tommitah <reijotahvanainen15@gmail.com>
2022-10-27 16:16:51 +0300

    part9: fixed the issue with casting

--- osa9/express-ts/index.ts
+++ osa9/express-ts/index.ts
@@ -7,16 +7,18 @@ import { calculateBMI } from "./calculateBMI"
 
 const app = express()
 
+app.use(queryType.middleware())
+app.set('query parser', (str: string) => qs.parse(str))
+
 app.get('/bmi', (req, res) => {
 	const { height, weight } = req.query
 	if (typeof height !== 'number' || typeof weight !== 'number')
 		res.status(400).json({ error: 'malformatted parameters' })
 
-	// tsserver(lsp) is being annoying and crying about the params not being numbers and preventing compilation
-	// typeof shows that they are numbers so idk
-	// casting to fix it, really annoying I must say
-	// there should be no situation where we get here but typeof height || number !== 'number'
-	const bmi: string = calculateBMI(height, weight)
+	// Neither the compiler nor tsserver knows about middleware at this point,
+	// so we get an annoying error concerning the qs-type.
+	// Casting fixes it, since the validation above catches any actual problems.
+	const bmi: string = calculateBMI(Number(height), Number(weight))
 	const result: object = {
 		height,
 		weight,
@@ -24,10 +26,6 @@ app.get('/bmi', (req, res) => {
 	}
 	res.status(200).json(result)
 })
-
-
-app.use(queryType.middleware())
-app.set('query parser', (str: string) => qs.parse(str))
 
 const PORT: number = 3003
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
